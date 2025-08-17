29c22c0837b5fd1d4bf8472761ed885baf07bce6
qfsdsg <zhongdou19990114@163.com>
2025-08-18 01:05:07 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -3,11 +3,6 @@ from bs4 import BeautifulSoup
 import re
 import os
 
-# 目标URL列表
-urls = ['https://api.uouin.com/cloudflare.html', 
-        'https://ip.164746.xyz'
-        ]
-
 # 正则表达式用于匹配IP地址
 ip_pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
 # 6.73mb/s
@@ -17,19 +12,27 @@ speed_pattern = r'\d+\.\d+[Mm][Bb]/s'
 if os.path.exists('ip.txt'):
     os.remove('ip.txt')
 
+if os.path.exists('speed.txt'):
+    os.remove('speed.txt')
+
 # 创建一个文件来存储IP地址
-with open('ip.txt', 'w') as file:
+def extract_ips():
+    result = {}
+    # 目标URL列表
+    urls = ['https://api.uouin.com/cloudflare.html','https://ip.164746.xyz']
     for url in urls:
-        # 发送HTTP请求获取网页内容
-        response = requests.get(url)
-        
+        try:
+            # 发送HTTP请求获取网页内容
+            response = requests.get(url, timeout=10)
+        except requests.exceptions.RequestException as e:
+            print(f"请求网站 [{url}] 发生错误: {e}")
+            continue
+
         # 使用BeautifulSoup解析HTML
         soup = BeautifulSoup(response.text, 'html.parser')
         
         # 根据网站的不同结构找到包含IP地址的元素
-        if url == 'https://api.uouin.com/cloudflare.html':
-            elements = soup.find_all('tr')
-        elif url == 'https://ip.164746.xyz':
+        if url == 'https://api.uouin.com/cloudflare.html' or url == 'https://ip.164746.xyz':
             elements = soup.find_all('tr')
         else:
             elements = soup.find_all('li')
@@ -42,7 +45,22 @@ with open('ip.txt', 'w') as file:
             if len(ip_matches) > 0 and len(speed_matches) > 0:
                 speed_matches = float(speed_matches[0].replace("MB/s", "").replace("mb/s", ""))
                 if speed_matches > 3.0:
-                    file.write(ip_matches[0] + '\n')
-                    # file.write(f"ip地址: {ip_matches[0]}, 速度: {speed_matches}" + '\n')
-                    
+                    result[ip_matches[0]] = {
+                        "url": url,
+                        "speed": speed_matches
+                        }
+    return result
+
+data = extract_ips()
+with open('ip.txt', 'w') as file:
+    for key, value in data.items():
+        file.write(f"{key}\n")
+
+with open('speed.txt', 'w') as file:
+    for key, value in data.items():
+        file.write(f"{value['url']}: {key} -- {value['speed']}mb/s\n")
+
+
+
+
 print('IP地址已保存到ip.txt文件中。')
